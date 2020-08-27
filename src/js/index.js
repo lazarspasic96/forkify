06630ea4a61e8d38dd25de317d69577662114f3a
Laks96 <lazarspasic96@gmail.com>
2020-08-27 20:16:29 +0200

    Setup modern babel env

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 console.log('eeee')
+
+import x from './testmodule'
+
+console.log(`babe work just too good ${x}`)
--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,19 @@ module.exports = {
             template: 'src/index.html',
             filename: 'index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
 }

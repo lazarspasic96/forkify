8c64b072225dab9baad60efcaf55143f222e732e
Laks96 <lazarspasic96@gmail.com>
2020-09-02 17:58:01 +0200

    Update pagination buttons

--- src/shared/paginationButton.js
+++ src/shared/paginationButton.js
@@ -2,11 +2,11 @@
 export const button = (type, page) => {
     const button = `
     <button class="btn-inline results__btn--${type}" data-pagenum = ${type === 'next' ? page + 1 : page - 1}>
-    <svg class="search__icon">
+        <span>Page${type === 'next' ? page + 1 : page - 1}</span>
+        <svg class="search__icon">
         <use href="img/icons.svg#icon-triangle-${type === 'next' ? 'right' : 'left'}"></use>
-    </svg>
-    <span>Page${type === 'next' ? page + 1 : page - 1}</span>
-</button> `
+        </svg>
+    </button> `
 
     return button
 }
